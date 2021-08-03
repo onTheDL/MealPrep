098a86364ea89ed6b4986d7b71c4b0263a6e11b5
Daniel Laserna <daniel.laserna@protonmail.com>
2021-08-03 19:43:07 -0400

    Add icons to bottom navigator, add active tint color

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -2,7 +2,8 @@ import React from "react";
 import { Platform } from "react-native";
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
-import { createBottomTabNavigator } from "react-navigation-tabs"
+import { createBottomTabNavigator } from "react-navigation-tabs";
+import { Ionicons } from "@expo/vector-icons";
 
 import Colors from "../constants/Colors";
 
@@ -44,9 +45,42 @@ const MealsNavigator = createStackNavigator(
   }
 );
 
-const MealsFavTabNavigator = createBottomTabNavigator({
-  Meals: MealsNavigator,
-  Favourites: FavouritesScreen,
-})
+const MealsFavTabNavigator = createBottomTabNavigator(
+  {
+    Meals: {
+      screen: MealsNavigator,
+      navigationOptions: {
+        tabBarIcon: (tabInfo) => {
+          return (
+            <Ionicons
+              name="ios-restaurant"
+              size={24}
+              color={tabInfo.tintColor}
+            />
+          );
+        },
+      },
+    },
+    Favourites: {
+      screen: FavouritesScreen,
+      navigationOptions: {
+        tabBarIcon: (tabInfo) => {
+          return (
+            <Ionicons
+              name="ios-star"
+              size={24}
+              color={tabInfo.tintColor}
+            />
+          );
+        },
+      },
+    },
+  },
+  {
+    tabBarOptions: {
+      activeTintColor: Colors.accentColor,
+    },
+  }
+);
 
 export default createAppContainer(MealsFavTabNavigator);
