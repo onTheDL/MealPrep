0f6036b10484cb2e637c226211e69a7a6cda8f4d
Daniel Laserna <daniel.laserna@protonmail.com>
2021-08-10 01:17:45 -0400

    Style the Drawer

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -103,11 +103,26 @@ const MealsFavTabNavigator =
 const FiltersNavigator = createStackNavigator({
   Filters: FiltersScreen,
 }, {
+  // navigationOptions: {
+  //   drawerLabel: "Filters!!!",
+  // },
   defaultNavigationOptions: defaultStackNavOptions
 });
 const MainNavigator = createDrawerNavigator({
-  MealsFavs: MealsFavTabNavigator,
+  MealsFavs: {
+    screen: MealsFavTabNavigator,
+    navigationOptions: {
+      drawerLabel: "Meals",
+    }
+  },
   Filters: FiltersNavigator,
+}, {
+  contentOptions: {
+    activeTintColor: Colors.accentColor,
+    labelStyle: {
+      fontFamily: "open-sans-bold",
+    }
+  }
 });
 
 export default createAppContainer(MainNavigator);
