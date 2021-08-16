3ff1caf480576a62628c1092cd56613c2e6cb0c6
Daniel Laserna <daniel.laserna@protonmail.com>
2021-08-16 19:56:29 -0400

    Add MealDetail Screen Content

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,10 +1,25 @@
 import React from "react";
-import { StyleSheet, View, Text, Button } from "react-native";
+import {
+  StyleSheet,
+  View,
+  Text,
+  Button,
+  ScrollView,
+  Image,
+} from "react-native";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 import CustomHeaderButton from "../components/HeaderButton";
+import DefaultText from "../components/DefaultText";
 
 import { MEALS } from "../data/dummy-data";
 // import Meal from '../models/meal'
+
+const ListItem = props => {
+  return <View style={styles.listItem}>
+    <DefaultText>{props.children}</DefaultText>
+  </View>
+}
+
 
 export default function MealDetailScreen({ navigation }) {
   const mealId = navigation.getParam("mealId");
@@ -12,11 +27,35 @@ export default function MealDetailScreen({ navigation }) {
   const selectedMeal = MEALS.find((meal) => meal.id === mealId);
 
   return (
-    <View style={styles.screen}>
-      <Text>MealDetailScreen</Text>
-      <Text>{selectedMeal.title}</Text>
-      <Button title="Go to Categories" onPress={() => navigation.popToTop()} />
-    </View>
+    <ScrollView>
+      <Image source={{ uri: selectedMeal.imageUrl }} style={styles.image} />
+
+      {/* Details of recipe */}
+      <View style={styles.details}>
+        <DefaultText>{selectedMeal.duration}m</DefaultText>
+        <DefaultText>{selectedMeal.complexity.toUpperCase()}</DefaultText>
+        <DefaultText>{selectedMeal.affordability.toUpperCase()}</DefaultText>
+      </View>
+
+      <Text style={styles.title}>Ingredients</Text>
+      {selectedMeal.ingredients.map((ingredient) => (
+        <ListItem key={ingredient}>{ingredient}</ListItem>
+      ))}
+
+      <Text style={styles.title}>Steps</Text>
+      {selectedMeal.steps.map((step) => (
+        <ListItem key={step}>{step}</ListItem>
+      ))}
+
+      {/* <View style={styles.screen}>
+        <Text>MealDetailScreen</Text>
+        <Text>{selectedMeal.title}</Text>
+        <Button
+          title="Go to Categories"
+          onPress={() => navigation.popToTop()}
+        />
+      </View> */}
+    </ScrollView>
   );
 }
 
@@ -38,9 +77,26 @@ MealDetailScreen.navigationOptions = (navigationData) => {
 };
 
 const styles = StyleSheet.create({
-  screen: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+  image: {
+    width: "100%",
+    height: 200,
+  },
+  details: {
+    flexDirection: "row",
+    padding: 15,
+    justifyContent: "space-around",
+  },
+  title: {
+    fontFamily: "open-sans-bold",
+    fontSize: 22,
+    textAlign: "center",
+    paddingTop: 18,
+  },
+  listItem: {
+    marginVertical: 10,
+    marginHorizontal: 20,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    padding: 10,
   },
 });
